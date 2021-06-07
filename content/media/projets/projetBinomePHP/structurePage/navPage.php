f9bdc9a7a6469c01be33caac68a9f3d3eee20533
Jean-Laily <jean.yansin@gmail.com>
2021-06-07 15:55:16 +0400

    Refonte des dossier projet

--- content/media/projets/projetBinomePHP/structurePage/navPage.php
+++
@@ -1,9 +0,0 @@
-<div class="navBloc">
-	<nav class="navBar">
-		<ul class="blocNavUl">
-			<li class="blocNavLi"><a href="./index.php?choixPage=1">Les Formations</a></li>
-			<li class="blocNavLi"><a href="./index.php?choixPage=2">Nos Vienoisseries</a></li>
-			<li class="blocNavLi"><a href="./index.php?choixPage=3">Nos Formateurs</a></li>
-		</ul>
-	</nav>
-</div>
